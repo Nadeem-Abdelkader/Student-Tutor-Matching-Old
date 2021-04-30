e80d582f6f8595a32d509dc695239785286a1288
nabd0018 <nabd0018@student.monash.edu>
2021-04-30 04:10:22 +0200

    updating application.py

--- System/application.py
+++ System/application.py
@@ -1,18 +1,12 @@
 from login import Login
-from user_collection import UserCollection
 
 
 class Application:
     API_KEY = "mPRM67bLTWDwchrMCtBCrWbh89tQb6"
-    USERS = UserCollection(API_KEY)
 
     def __init__(self):
         a = Login(self.API_KEY)
         a.login()
-        # jwt, uname = a.login()
-        # if jwt is not None:
-        #     s = SelectAction(self.USERS.getUserByUsername(uname))
-        #     s.start()
 
 
 if __name__ == "__main__":
