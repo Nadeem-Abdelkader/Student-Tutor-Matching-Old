b28880d69fa029e74317b6c8ed2d40628ef2fbc1
nabd0018 <nabd0018@student.monash.edu>
2021-05-01 03:53:31 +0200

    updating new_request_details.py

--- System/new_request_details.py
+++ System/new_request_details.py
@@ -4,11 +4,9 @@ from tkinter import *
 class NewRequestDetails:
     window = None
     json_data = None
-    additional_info = None
 
-    def __init__(self, json_data, additional_info):
+    def __init__(self, json_data):
         self.json_data = json_data
-        self.additional_info = additional_info
 
     def start(self):
         self.window = Tk()
@@ -25,10 +23,10 @@ class NewRequestDetails:
         t.insert(INSERT, str("Date Created: ") + str(self.json_data['dateCreated']) + str("\n"))
         t.insert(INSERT, str("Date Closed: ") + str(self.json_data['dateClosedDown'])+ str("\n"))
         t.insert(INSERT, str("Subject: ") + str(self.json_data['subject']['name'])+ str("\n"))
-        t.insert(INSERT, str("Competency: ") + str(self.additional_info['competency'])+ str("\n"))
-        t.insert(INSERT, str("Hours per Week: ") + str(self.additional_info['hours_per_week'])+ str("\n"))
-        t.insert(INSERT, str("Sessions per Week: ") + str(self.additional_info['sessions_per_week'])+ str("\n"))
-        t.insert(INSERT, str("Rate per Session: ") + str(self.additional_info['rate_per_session']))
+        t.insert(INSERT, str("Competency: ") + str(self.json_data['additionalInfo']['competency'])+ str("\n"))
+        t.insert(INSERT, str("Hours per Week: ") + str(self.json_data['additionalInfo']['hours_per_week'])+ str("\n"))
+        t.insert(INSERT, str("Sessions per Week: ") + str(self.json_data['additionalInfo']['sessions_per_week'])+ str("\n"))
+        t.insert(INSERT, str("Rate per Session: ") + str(self.json_data['additionalInfo']['rate_per_session']))
 
 
         Label(self.window, text="Current Bids: ").grid(row=4, column=0)
